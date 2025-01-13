9075bb5953eba6fdb72fd24b8ebd2cd392148d93
larisa1903 <munteanu.larisa.f2y@student.ucv.ro>
2025-01-14 00:39:36 +0200

    Update profesor.h

--- profesor.h
+++ profesor.h
@@ -1,34 +1,48 @@
-﻿#pragma once
+#ifndef PROFESOR_H
+#define PROFESOR_H
+
 #include <string>
 #include <vector>
-
-class Curs; // Forward declaration
+#include "curs.h" 
 
 class Profesor {
 private:
     std::string nume;
+    std::string prenume;
     std::string username;
     std::string password;
     std::vector<Curs*> cursuriPredate;
 
 public:
-    Profesor(std::string nume, std::string username, std::string password)
-        : nume(nume), username(username), password(password) {}
+    Profesor(std::string nume, std::string prenume, std::string username, std::string password)
+        : nume(nume), prenume(prenume), username(username), password(password) {}
+
+    std::string getNume() const {
+        return nume;
+    }
+
+    std::string getPrenume() const {
+        return prenume;
+    }
 
     std::string getUsername() const {
         return username;
     }
-   
-    std::string getPassword() const { return password; }  // Getter pentru parola
+
+    std::string getPassword() const {
+        return password;
+    }
 
     void adaugaCurs(Curs* curs) {
         cursuriPredate.push_back(curs);
     }
 
     void afisareCursuriPredate() const {
-        std::cout << "Profesorul " << nume << " preda urmatoarele cursuri:\n";
+        std::cout << "Profesorul " << nume << " " << prenume << " preda urmatoarele cursuri:\n";
         for (const auto& curs : cursuriPredate) {
             std::cout << "- " << curs->getNumeCurs() << std::endl;
         }
     }
 };
+
+#endif  // PROFESOR_H
